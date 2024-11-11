b4de975304e820a908ef458e4b9504066a6b66a5
sean <jh9826@jbnu.ac.kr>
2024-11-11 23:33:33 +0900

    initial project

---
+++ src/main/java/com/voicecut/api/VoicecutApiApplication.java
@@ -0,0 +1,13 @@
+package com.voicecut.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicecutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoicecutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voicecut/api/VoicecutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voicecut.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicecutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
