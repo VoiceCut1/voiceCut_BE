6471e67a3b6465a17abb13e7c7829061af12455b
sean <jh9826@jbnu.ac.kr>
2024-11-14 15:28:00 +0900

    feat: add llama responese and controller

---
+++ src/main/java/com/voicecut/api/llama/response/LlamaResponse.java
@@ -0,0 +1,10 @@
+package com.voicecut.api.llama.response;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class LlamaResponse {
+    private String message;
+}
