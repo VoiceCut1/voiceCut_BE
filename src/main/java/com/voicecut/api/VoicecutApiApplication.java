c2cc61e21092ec7330bd456e05df210f06d5de5f
sean <jh9826@jbnu.ac.kr>
2024-11-16 04:05:29 +0900

    feat: Deactivate db

--- src/main/java/com/voicecut/api/VoicecutApiApplication.java
+++ src/main/java/com/voicecut/api/VoicecutApiApplication.java
@@ -2,8 +2,12 @@ package com.voicecut.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class VoicecutApiApplication {
 
 	public static void main(String[] args) {
