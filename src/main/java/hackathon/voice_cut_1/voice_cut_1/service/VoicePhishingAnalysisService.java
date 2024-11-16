0708cd3f81feca8ebe91c14c786c797c630e95a4
49EHyeon42 <49ehyeon42@gmail.com>
2024-11-16 16:54:01 +0900

    refactor: SMS 전송 로직 분리, 비동기 처리로 변경

--- src/main/java/hackathon/voice_cut_1/voice_cut_1/service/VoicePhishingAnalysisService.java
+++ src/main/java/hackathon/voice_cut_1/voice_cut_1/service/VoicePhishingAnalysisService.java
@@ -2,17 +2,8 @@ package hackathon.voice_cut_1.voice_cut_1.service;
 
 import hackathon.voice_cut_1.voice_cut_1.entity.Elder;
 import hackathon.voice_cut_1.voice_cut_1.exception.ElderNotFoundException;
-import hackathon.voice_cut_1.voice_cut_1.exception.SmsSendFailedException;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.exception.NurigoEmptyResponseException;
-import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
-import net.nurigo.sdk.message.exception.NurigoUnknownException;
-import net.nurigo.sdk.message.model.Message;
-import net.nurigo.sdk.message.service.DefaultMessageService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,25 +17,7 @@ public class VoicePhishingAnalysisService {
 
     private final RedisTemplate<String, Object> redisTemplate;
     private final OpenAiService openAiService;
-
-    @Value("${coolsms.key}")
-    private String coolsmsKey;
-
-    @Value("${coolsms.secret-key}")
-    private String coolsmsSecretKey;
-
-    @Value("${coolsms.calling-number}")
-    private String coolsmsCallingNumber;
-
-    @Value("${coolsms.domain}")
-    private String coolsmsDomain;
-
-    private DefaultMessageService messageService;
-
-    @PostConstruct
-    public void init() {
-        messageService = NurigoApp.INSTANCE.initialize(coolsmsKey, coolsmsSecretKey, coolsmsDomain);
-    }
+    private final SmsService smsService;
 
     public void analysisVoicePhishing(
             String uuid,
@@ -69,32 +42,10 @@ public class VoicePhishingAnalysisService {
                     // TODO: FCM 로직 추기
 
                     if (percent >= 90 && !elder.isSendMessageAt90Percent()) {
-                        sendSmsToGuardianNumbers(elder.getNickname(), elder.getGuardianNumbers());
-
-                        redisTemplate.opsForValue().set(uuid, new Elder(elder.getNickname(), elder.getGuardianNumbers(), true));
+                        // TODO: 예외 처리 추가
+                        smsService.sendSmsToGuardianNumbersAsync(elder.getNickname(), elder.getGuardianNumbers())
+                                .thenAccept(ignored -> redisTemplate.opsForValue().set(uuid, new Elder(elder.getNickname(), elder.getGuardianNumbers(), true)));
                     }
                 });
-    }
-
-    private void sendSmsToGuardianNumbers(
-            String nickname,
-            Collection<String> guardianNumbers
-    ) {
-        List<Message> messages = new ArrayList<>();
-
-        for (String guardianNumber : guardianNumbers) {
-            Message message = new Message();
-            message.setFrom(coolsmsCallingNumber);
-            message.setTo(guardianNumber);
-            message.setText("[음성감독원] " + nickname + " 어르신이 보이스 피싱을 당하고 있습니다!");
-
-            messages.add(message);
-        }
-
-        try {
-            messageService.send(messages);
-        } catch (NurigoMessageNotReceivedException | NurigoEmptyResponseException | NurigoUnknownException exception) {
-            throw new SmsSendFailedException();
-        }
     }
 }
