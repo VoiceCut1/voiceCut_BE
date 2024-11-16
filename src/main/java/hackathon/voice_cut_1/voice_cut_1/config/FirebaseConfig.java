2197bc772c32564322cf6fc42e7f373d76f48835
[lky3004me@gmail.com] <lky3004me@gmail.com>
2024-11-16 14:05:44 +0900

    chore: alter imports

--- src/main/java/hackathon/voice_cut_1/voice_cut_1/config/FirebaseConfig.java
+++ src/main/java/hackathon/voice_cut_1/voice_cut_1/config/FirebaseConfig.java
@@ -1,5 +1,8 @@
 package hackathon.voice_cut_1.voice_cut_1.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/hackathon/voice_cut_1/voice_cut_1/controller/ScoreController.java
+++ src/main/java/hackathon/voice_cut_1/voice_cut_1/controller/ScoreController.java
@@ -1,6 +1,6 @@
 package hackathon.voice_cut_1.voice_cut_1.controller;
 
-import com.example.voicecut1.service.NotificationService;
+import hackathon.voice_cut_1.voice_cut_1.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
