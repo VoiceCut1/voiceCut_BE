de057e2621da32c5312ec7599ed52f84d176bd50
49EHyeon42 <49ehyeon42@gmail.com>
2024-11-18 15:04:46 +0900

    fix: SMS 내용 수정

--- src/main/java/hackathon/voice_cut_1/voice_cut_1/service/SmsService.java
+++ src/main/java/hackathon/voice_cut_1/voice_cut_1/service/SmsService.java
@@ -37,7 +37,8 @@ public class SmsService {
             Message message = new Message();
             message.setFrom(coolsmsCallingNumber);
             message.setTo(guardianNumber);
-            message.setText("[" + nickname + "]님이 지금하고 계신 통화가 보이스피싱으로 매우 의심됩니다!\n주요 통화 내용 : " + text);
+            message.setText("노노아 테스트");
+//            message.setText("[" + nickname + "]님이 지금하고 계신 통화가 보이스피싱으로 매우 의심됩니다!\n주요 통화 내용 : " + text);
 
             messages.add(message);
         }
